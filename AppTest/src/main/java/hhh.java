c6a460716698102dec3595b10ebdeafeeaca4d88
86151 <15161536514@163.com>
2020-01-13 15:15:45 +0800

    test

--- AppTest/src/main/java/hhh.java
+++ AppTest/src/main/java/hhh.java
@@ -1,5 +1,5 @@
 public class hhh {
     public static void main(String[] args) {
-        System.out.println(" fafasf");
+        System.out.println(" aaaaaaaaaaaaaaaaaa");
     }
 }
--- AppTest/src/main/java/hiworld.java
+++ AppTest/src/main/java/hiworld.java
@@ -1,5 +1,5 @@
 public class hiworld {
     public static void main(String[] args) {
-        System.out.println("hiworsdgsdgsdgld");
+        System.out.println("asdasdasdsa");
     }
 }
